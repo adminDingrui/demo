013889febc132b38f9e70105c290d36a38a68f68
adminDingRui <1056718773@qq.com>
2020-09-16 14:57:41 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new A();
+        A b = new B();
+        A c = new C();
+        adfdsfds
+
+
+
+    }
+    
+
+}
